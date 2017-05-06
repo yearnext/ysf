47fa93259e36acd559b80fd30337bf96cbed9cb9
yearnext <1668501573@qq.com>
2017-05-06 20:43:59 +0800

    add debug component

---
+++ framework/fw_debug.h
@@ -0,0 +1,59 @@
+/**
+ ******************************************************************************
+ * @file       fw_debug.h
+ * @author     yearnext
+ * @version    1.0.0
+ * @date       2017-01-10
+ * @brief      framework debug head files
+ * @par        paltform
+ *                 Windows
+ * @par        compiler
+ *                 GCC
+ ******************************************************************************
+ * @note
+ * 1.XXXXX
+ ******************************************************************************
+ */
+
+/**
+ * @defgroup framework debug component
+ * @{
+ */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __FRAMEWORK_DEBUG_H__
+#define __FRAMEWORK_DEBUG_H__
+
+/* Add c++ compatibility------------------------------------------------------*/
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "fw_core.h"
+
+/* Framework debug -----------------------------------------------------------*/
+/**
+ *******************************************************************************
+ * @brief      debug config
+ *******************************************************************************
+ */    
+#if USE_FRAMEWORK_DEBUG
+// #include "bsp_uart.h"
+
+// #define log(a)     bsp_uart_tx_config(a, sizeof(a))
+#define log(a)
+#else
+#define log(a)
+#endif
+                             
+/* Add c++ compatibility------------------------------------------------------*/
+#ifdef __cplusplus
+}
+#endif
+	
+#endif       /** end include define */
+
+/** @}*/     /** framework debug */
+
+/**********************************END OF FILE*********************************/
